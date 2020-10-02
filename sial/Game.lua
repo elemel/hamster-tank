a390a3c2e33a0f447ede78abab6045e8059d550d
Mikael Lind <elemel@elemel.se>
2020-10-03 01:47:52 +0200

    Fixed time step

--- sial/Game.lua
+++ sial/Game.lua
@@ -3,10 +3,21 @@ local Class = require("sial.Class")
 local M = Class.new()
 
 function M:init()
+  self.fixedDt = 1 / 60
+  self.accumulatedDt = 0
   self.cameraTransform = love.math.newTransform()
 end
 
 function M:update(dt)
+  self.accumulatedDt = self.accumulatedDt + dt
+
+  while self.accumulatedDt >= self.fixedDt do
+    self.accumulatedDt = self.accumulatedDt - self.fixedDt
+    self:fixedUpdate(self.fixedDt)
+  end
+end
+
+function M:fixedUpdate(dt)
 end
 
 function M:draw()
